b43841490441d93b8e5490a601887f790561e342
CHI-CHE LIAO <91250134+chiche0306@users.noreply.github.com>
2021-10-07 17:40:56 +0800

    Add files via upload

---
+++ week04/week04-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float  c=3.141592685951;
+    double d=3.141592658292;
+    printf("%f\n",c);
+    printf("%f\n",d);
+    printf("%.5f\n",c);
+    printf("%.10f\n",d);
+    printf("%.11f\n",c);
+}
