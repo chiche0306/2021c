d8cf21fc6c47e912cbb4ab5f293a115e5b58d08e
CHI-CHE LIAO <91250134+chiche0306@users.noreply.github.com>
2021-10-28 17:22:00 +0800

    Add files via upload

---
+++ week07-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n=3;
+
+    if (n>0){
+        printf ("n大於0ㄛ\n");
+    }
+    while(n>0){
+        printf ("while (判斷n>0)有進來ㄛ\n");
+    }
+}
---
+++ week07-5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    for (int i=0; i<3; i++){
+        printf ("電腦的迴圈的最好寫法:%d\n", i);
+    }
+    for (int i=1; i<=3; i++){
+        printf ("人類的數字,從1數:%d\n", i);
+    }
+
+}
+
